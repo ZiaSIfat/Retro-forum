4eb14816cda09b8a44e17a9751e17ee160676bfa
ZiaSifat <ziasifat150@gmail.com>
2024-09-14 02:39:27 +0600

    lates posts added

--- js/index.js
+++ js/index.js
@@ -122,9 +122,8 @@ const markPost = (post) =>{
 
 
 const setStatus = (post,postCard) =>{
-    console.log(post);
+    // console.log(post);
     const statusDiv = postCard.querySelector('.status-indicator');
-        console.log(statusDiv);
         
         if(post.isActive){
             statusDiv.classList.remove('inactive')
@@ -148,6 +147,56 @@ const searchPost = () =>{
     })
     console.log(searchtext);
     loadPosts(searchtext);
+}
+
+function loadLatest(){
+    fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+    .then(res => res.json())
+    .then(data => displayLatest(data))
+}
+
+loadLatest();
+
+const displayLatest = (posts) => {
+      for(post of posts){
+        console.log(post);
+
+        const latestDiv = document.getElementById('latest-div');
+        const latestPost = document.createElement('div');
+        latestPost.classList = 'card card-compact w-96 shadow-xl bg-white mt-7';
+        latestPost.innerHTML = `
+        <figure>
+        <img
+          src=${post.cover_image}
+          alt="Shoes" class="p-8"/>
+      </figure>
+      <div class="card-body">
+          <div class='flex gap-2'>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+  <path d="M12 11.993a.75.75 0 0 0-.75.75v.006c0 .414.336.75.75.75h.006a.75.75 0 0 0 .75-.75v-.006a.75.75 0 0 0-.75-.75H12ZM12 16.494a.75.75 0 0 0-.75.75v.005c0 .414.335.75.75.75h.005a.75.75 0 0 0 .75-.75v-.005a.75.75 0 0 0-.75-.75H12ZM8.999 17.244a.75.75 0 0 1 .75-.75h.006a.75.75 0 0 1 .75.75v.006a.75.75 0 0 1-.75.75h-.006a.75.75 0 0 1-.75-.75v-.006ZM7.499 16.494a.75.75 0 0 0-.75.75v.005c0 .414.336.75.75.75h.005a.75.75 0 0 0 .75-.75v-.005a.75.75 0 0 0-.75-.75H7.5ZM13.499 14.997a.75.75 0 0 1 .75-.75h.006a.75.75 0 0 1 .75.75v.005a.75.75 0 0 1-.75.75h-.006a.75.75 0 0 1-.75-.75v-.005ZM14.25 16.494a.75.75 0 0 0-.75.75v.006c0 .414.335.75.75.75h.005a.75.75 0 0 0 .75-.75v-.006a.75.75 0 0 0-.75-.75h-.005ZM15.75 14.995a.75.75 0 0 1 .75-.75h.005a.75.75 0 0 1 .75.75v.006a.75.75 0 0 1-.75.75H16.5a.75.75 0 0 1-.75-.75v-.006ZM13.498 12.743a.75.75 0 0 1 .75-.75h2.25a.75.75 0 1 1 0 1.5h-2.25a.75.75 0 0 1-.75-.75ZM6.748 14.993a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1-.75-.75Z" />
+  <path fill-rule="evenodd" d="M18 2.993a.75.75 0 0 0-1.5 0v1.5h-9V2.994a.75.75 0 1 0-1.5 0v1.497h-.752a3 3 0 0 0-3 3v11.252a3 3 0 0 0 3 3h13.5a3 3 0 0 0 3-3V7.492a3 3 0 0 0-3-3H18V2.993ZM3.748 18.743v-7.5a1.5 1.5 0 0 1 1.5-1.5h13.5a1.5 1.5 0 0 1 1.5 1.5v7.5a1.5 1.5 0 0 1-1.5 1.5h-13.5a1.5 1.5 0 0 1-1.5-1.5Z" clip-rule="evenodd" />
+</svg>
+
+          <p>${post.author.posted_date ? post.author.posted_date : 'No Publish Date'}</p>
+          </div>
+          
+        <h2 class="card-title font-bold">${post.title}</h2>
+        <p>${post.description}</p>
+        <div class='flex gap-2'>
+          <div class='w-10 h-15'>
+          <img class='rounded-3xl'  src=${post.profile_image} alt=""></div>
+          <div>
+              <p class='font-bold'>${post.author.name}</p>
+              <p>${post.author.designation ? post.author.designation : 'Unknown'}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+        
+        `
+
+        latestDiv.appendChild(latestPost);
+      }
 }
 
 
